3cc7729014a2f7d92f18e97e18eb83f9ba59a800
Paulo Guidolin <123969047+paulog16@users.noreply.github.com>
2023-08-17 17:56:36 -0300

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-kc1=float(input('cuantos kilometros puedes recorrer con un litro de combustible'))
-capacidad=int(input('que capacidad tiene el tanque '))
-kilometros=int(input('cuantos kilometros recorreran'))
-tanquesN=(kilometros/kc1)/capacidad
-print(f'tanques necesarios para el viaje: {tanquesN}')
