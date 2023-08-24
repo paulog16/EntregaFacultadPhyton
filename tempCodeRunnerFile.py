46f6ebdeec27220b0ccaf3c7aa9edf90be47c138
paulo guidolin <pguidolin0@gmail.com>
2023-08-24 15:27:10 -0300

    temp eliminado

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-tipopizza=input('desea una pizza vegetariana o no?')
-if tipopizza=='si':
-    print('INGREDIENTES:PIMIENTO,TOFU ')
-    ing=input('ingrese el ingrediente que desea: ')
-    print(f'usted eligio una pizza vegana con {ing}')  
-else:
-    print('INGREDIENTES:PEPERONI,JAMON,SALOMON ')
-    ing=input('ingrese el ingrediente que desea: ')
-    print(f'usted eligio una pizza no vegetariana con {ing}')
