00967f7dc58020ae92b7aaac9d24470fdad454b1
PauloGuidolin <pguidolin0@gmail.com>
2023-08-29 11:43:15 -0300

    ejercicio iteraciones

---
+++ ejercicio_ciclos.py
@@ -0,0 +1,27 @@
+arregloM=[]
+numCorrer=int(input('cuantos lugares quieres correr: '))
+for i in range(1,6):
+    mensaje=input(f'ingrese el mensaje al ofical{i}: ')
+    arregloM.append(mensaje)
+   
+print(arregloM)    
+abecedario='abcdefghijklmnopqrstuvwxyz'
+arregloPalabras=[]
+for p in arregloM:
+    msj_enc = ''
+    for letra in p:
+        if letra in abecedario:
+            ind=abecedario.find(letra)
+            ind = (ind+numCorrer)%26
+            msj_enc += abecedario[ind] 
+        else:
+              msj_enc=letra   
+    arregloPalabras.append(msj_enc)
+print(arregloPalabras)   
+
+
+        
+        
+
+
+
