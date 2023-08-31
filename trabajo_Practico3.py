9f982a9fd449e3f6855e030b7314822c45fe83ff
paulo guidolin <pguidolin0@gmail.com>
2023-08-31 14:22:49 -0300

    en proceso

--- trabajo_Practico3.py
+++ trabajo_Practico3.py
@@ -29,14 +29,50 @@ for p in range(1,ages):
     cashInverted+=cashInverted*interestAnual
     print(f'capital obtenido {cashInverted}')
 
-# 7-	Escribir un programa que muestre por pantalla las tablas de multiplicar del 1 al 10.
+# 6-	Escribir un programa que pida al usuario un número entero y muestre por pantalla un triángulo rectángulo como el de más abajo, de altura el número introducido.
     hight=int(input('ingrese altura: '))
     for i in range(1, altura + 1):
         print('*' * i)
 
-# 7-	Escribir un programa que muestre por pantalla las tablas de multiplicar del 1 al 10.
+# 7-Escribir un programa que muestre por pantalla las tablas de multiplicar del 1 al 10.
 
 for i in range(1):
     print('tabla del 1')
     for j in range(1,10):
         print(f'{i} x {j} es : {i*j}')
+# 6-Escribir un programa que pida al usuario un número entero y muestre por pantalla un triángulo rectángulo como el de más abajo, de altura el número introducido.
+
+number=int(input('ingrese un numero: '))
+for i in range(1,number+1,2):           
+        for j in range(i,0,-2):
+            print(j,end='')
+        print('')
+
+
+#9-	Escribir un programa que almacene la cadena de caracteres contraseña en una variable, pregunte al usuario por la contraseña hasta que introduzca la contraseña correcta.
+password='Godoycruz'
+while True:
+    password2=input('ingrese contraseña: ')
+    if password2==password:
+        break
+
+print('contraseña correcta')
+#10-Escribir un programa que pida al usuario un número entero y muestre por pantalla si es un número primo o no.
+number=int(input('ingrese un numero: '))
+if (number%number==0)and(number%1==0):
+    print('primo')
+else:
+    print('no primo')  
+# 
+word=input('ingrese una palabra: ')
+for i in word[::-1]: #esto hace que lea de atras hacia adelante
+    print(i) 
+
+   
+
+
+
+    
+    
+
+
