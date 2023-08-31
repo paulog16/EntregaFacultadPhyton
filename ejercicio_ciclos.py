982c247f1483a2abc73fb17ddfaaa43b11fc3dde
PauloGuidolin <pguidolin0@gmail.com>
2023-08-31 10:29:45 -0300

    ejercicio teminado

--- ejercicio_ciclos.py
+++ ejercicio_ciclos.py
@@ -1,23 +1,23 @@
-# arregloM=[]
-# numCorrer=int(input('cuantos lugares quieres correr: '))
-# for i in range(1,6):
-#     mensaje=input(f'ingrese el mensaje al ofical{i}: ')
-#     arregloM.append(mensaje)
+arregloM=[]
+numCorrer=int(input('cuantos lugares quieres correr: '))
+for i in range(1,6):
+     mensaje=input(f'ingrese el mensaje al ofical{i}: ')
+     arregloM.append(mensaje)
 
-# print(arregloM)
-# abecedario='abcdefghijklmnopqrstuvwxyz'
-# arregloPalabras=[]
-# for p in arregloM:
-#     msj_enc = ''
-#     for letra in p:
-#         if letra in abecedario:
-#             ind=abecedario.find(letra)
-#             ind = (ind+numCorrer)%26
-#             msj_enc += abecedario[ind]
-#         else:
-#               msj_enc+=letra
-#     arregloPalabras.append(msj_enc)
-# print(arregloPalabras)
+print(arregloM)
+abecedario='abcdefghijklmnopqrstuvwxyz'
+arregloPalabras=[]
+for p in arregloM:
+     msj_enc = ''
+     for letra in p:
+         if letra in abecedario:
+             ind=abecedario.find(letra)
+             ind = (ind+numCorrer)%26
+             msj_enc += abecedario[ind]
+         else:
+               msj_enc+=letra
+     arregloPalabras.append(msj_enc)
+print(arregloPalabras)
 
 # punto2
 """Crear un programa que solicite el ingreso de números enteros positivos, hasta que el usuario ingrese el
