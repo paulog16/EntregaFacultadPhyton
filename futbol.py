3a4d5089b159af0ac37074b2ba09c27529b6fe68
PauloGuidolin <pguidolin0@gmail.com>
2023-10-10 11:51:51 -0300

    .

--- futbol.py
+++ futbol.py
@@ -1,25 +1,9 @@
-matriz=[[0,4,2,1],
-        [5,0,3,2],
-        [0,2,0,1],
-        [1,0,2,0]]
-print(matriz[0][1])
-contvicequipo1=0
-for i in range(len(matriz)):
-    for j in range(len(matriz)):
-       if i!=j: 
-            if i==0:
-                if matriz[i][j]<matriz[j][i]:
-                    print('gano el segundo')
-                elif matriz[i][j]>matriz[j][i]:
-                    print('gano el primero')   
-                    contvicequipo1+=1  
-                else:
-                    print('empate')
 
 def res(matriz,num):
     contvic=0                    
     contderr=0                    
-    contemp=0                    
+    contemp=0  
+    conpuntos=0                  
     for i in range(len(matriz)):
         for j in range(len(matriz)):
             if i!=j: 
@@ -28,23 +12,30 @@ def res(matriz,num):
                         
                         contderr+=1
                     elif matriz[i][j]>matriz[j][i]:
-                           
                         contvic+=1  
+                        conpuntos+=3
                     else:
-                        
+                        conpuntos+=1
                         contemp+=1
-    listaestadisticas=[contvic,contderr,contemp]
-    return listaestadisticas                    
+    listaestadisticas=[contvic,contderr,contemp,conpuntos]
+    return listaestadisticas
+                    
 matriz=[[0,4,2,1],
         [5,0,3,2],
         [0,2,0,1],
         [1,0,2,0]]
 
 estadisticasequipo1=res(matriz,0)
-print(f"Equipo 1: Victorias={estadisticasequipo1[0]}, Derrotas={estadisticasequipo1[1]}, Empates={estadisticasequipo1[2]}")
+print(f"Equipo 1: Victorias={estadisticasequipo1[0]}, Derrotas={estadisticasequipo1[1]}, Empates={estadisticasequipo1[2]}, puntosTotales= {estadisticasequipo1[3]}")
+
+
 estadisticasequipo2=res(matriz,1)
-print(f"Equipo 2: Victorias={estadisticasequipo2[0]}, Derrotas={estadisticasequipo2[1]}, Empates={estadisticasequipo2[2]}")
+print(f"Equipo 2: Victorias={estadisticasequipo2[0]}, Derrotas={estadisticasequipo2[1]}, Empates={estadisticasequipo2[2]},  puntosTotales= {estadisticasequipo2[3]}")
+
+
 estadisticasequipo3=res(matriz,2)
-print(f"Equipo 3: Victorias={estadisticasequipo3[0]}, Derrotas={estadisticasequipo3[1]}, Empates={estadisticasequipo3[2]}")
-estadisticasequipo3=res(matriz,3)
-print(f"Equipo 4: Victorias={estadisticasequipo3[0]}, Derrotas={estadisticasequipo3[1]}, Empates={estadisticasequipo3[2]}")
+print(f"Equipo 3: Victorias={estadisticasequipo3[0]}, Derrotas={estadisticasequipo3[1]}, Empates={estadisticasequipo3[2]}, puntosTotales= {estadisticasequipo3[3]}")
+
+
+estadisticasequipo4=res(matriz,3)
+print(f"Equipo 4: Victorias={estadisticasequipo4[0]}, Derrotas={estadisticasequipo4[1]}, Empates={estadisticasequipo4[2]}, puntosTotales= {estadisticasequipo4[3]}")
