06e81518ab8789ff510b9ca6fc81cdaa6a222615
paulo guidolin <pguidolin0@gmail.com>
2023-10-16 19:08:14 -0300

    .

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,10 +1,20 @@
-dic={}
-nombre=input('ingrese nombre: ')
-dic['nombre']=nombre
-edad=input('ingrese edad: ')
-dic['edad']=edad
-direccion=input('ingrese direccion: ')
-dic['direccion']=direccion
-telefono=input('ingrese telefono: ')
-dic['telefono']=telefono
-print(dic)
+libros = [
+    {"titulo": "Iron Man", "autor": "G.Paulo Guidolin", "año": 2002},
+    {"titulo": "Cenicienta", "autor": "Disney", "año": 1967},
+    {"titulo": "El extraño mundo de jack", "autor": "Sheckpeasre", "año": 1860},
+
+]
+
+# Función para ordenar la lista de libros en función de un campo específico
+def ordenar_libros_por_campo(libros, campo):
+    return sorted(libros, key=lambda x: x[campo])
+
+# Campo por el que deseamos ordenar los libros (en este caso, "año")
+campo_ordenamiento = "año"
+
+# Llamamos a la función de ordenamiento
+libros_ordenados = ordenar_libros_por_campo(libros, campo_ordenamiento)
+
+# Imprimimos la lista de libros ordenada
+for libro in libros_ordenados:
+    print(f"Título: {libro['titulo']}, Autor: {libro['autor']}, Año: {libro['año']}")
