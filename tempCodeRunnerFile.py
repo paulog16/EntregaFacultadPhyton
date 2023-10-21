d5727f1feb8d6edc5551d319353e477bb5ebd424
paulo guidolin <pguidolin0@gmail.com>
2023-10-21 12:01:36 -0300

    .

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,14 +1,20 @@
-def multiplo(a, b):
-    if a == 1: #si a es 1, es verdadero por que todos los numeros son multiplos de 1
-        return True
-    if a < b: # si b es mas grande que a es falso por que para que pueda ser multiplo tiene que ser mas chico
-        return False
-    if a % b == 0: # si a % b ==0 retorna a//b,b ej, 16//2,2 = 8,2, y vuelve a repetir el proceso
-        return multiplo(a//b, b)
-    return False
+def posiciones(a, b, start=0, positions=None):
+    if positions is None:
+        positions = []
 
+    if start >= len(a):
+        return positions
 
-a = 42
-b = 6
-resultado = multiplo(a, b)
-print(resultado)
+    index = a.find(b, start)
+
+    if index != -1:
+        positions.append(index)
+        return posiciones(a, b, index + 1, positions)
+    else:
+        return positions
+
+# Ejemplo de uso
+a = "un tete a tete con Tete"
+b = "te"
+result = posiciones(a, b)
+print(result)
